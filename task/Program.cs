5ba0549cd382205564f426ea2ad44224a615aa8f
sabir gadzhimuradov <89633677773@mail.ru>
2023-01-19 13:57:45 +0300

    sem5DZ

---
+++ task/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача.32 Напишите программу которая заменяет элементы массива
+// положительные элементы замените на соотвествующие отрицательные и наоборот
+
+int[] FillArrayWithRandom(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+
+{
+    array[i] = new Random().Next(-9, 10);
+}
+return array;
+}
+
+int[] array = new int[5];
+array = FillArrayWithRandom(array);
+Console.Write("Ваш случайный массив: ");
+Console.Write(string.Join(", ", array));
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1;
+}
+Console.WriteLine();
+Console.Write(string.Join(", ", array));
---
+++ task38DZ/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.7; 7.2; 2.1; 7.8] -> 5.7
+
+int size = 10;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Максимальное число: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
